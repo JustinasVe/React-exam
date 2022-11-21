6d0ddd0067f8efc50bde522ed2a601ac13b2f39e
unknown <justinasve@gmail.com>
2022-11-21 21:07:46 +0200

    added login page

--- src/views/LoginPage/LoginPage.js
+++ src/views/LoginPage/LoginPage.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
+import { BASE_URL } from "../../utils/constants";
+
+
 export const LoginPage = () => {
+
+    const [error, setError] = useState('');
 
     const handleLogin = (e) => {
         e.preventDefault();
-        console.log('login successful')
+        fetch(`${BASE_URL}/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: 'test123@gmail.com',
+                // password: 'test123'
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.err) {
+                setError(data.err);
+            } else {
+                localStorage.setItem("token", data.token);
+            }
+        })
     }
 
     return (
         <div>
+            {error && <h3>{error}</h3>}
             <form onSubmit={handleLogin} >
                 <input placeholder="Email" type={"email"} />
                 <input placeholder="Password" type={"password"} />
